bcf70181a82de893b7623b15c222bb9d635249c4
Harman <harman@gmail.com>
2020-02-07 21:19:01 +1300

    Add project files.

---
+++ Panda_Racing/Factory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Panda_Racing
+{
+    public class Factory
+    {
+        public Punter getPunter(String Name, Label MaximumBet, Label bet)
+        {
+            Punter p;
+            switch (Name.ToLower())
+            {
+                case "jassi":
+                    p = new Jassi(null, MaximumBet, 50, bet);
+                    break;
+
+                case "harman":
+                    p = new Harman(null, MaximumBet, 50, bet);
+                    break;
+
+                case "guri":
+                    p = new Guri(null, MaximumBet, 50, bet);
+                    break;
+
+                default:
+                    p = null;
+                    break;
+            }
+            p.setPunterName();
+            return p;
+        }
+    }
+}
